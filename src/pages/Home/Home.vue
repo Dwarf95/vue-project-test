c5d8b2c692e729965633dcd89c67017da8253ed6
Lazar Stankovic <lazar.stankovic@atessoft.rs>
2020-10-08 10:00:24 +0200

    Setting up vue router

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Hello world</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
