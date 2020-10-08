74b29a40beb1176f48684b4a1c752afbdce3e367
LukaJez <luka.jez55@gmail.com>
2020-10-08 15:07:04 +0200

    Sign Up validation

--- src/pages/SignUp/SignUp.vue
+++ src/pages/SignUp/SignUp.vue
@@ -1,93 +1,103 @@
 <template>
-    <div>
-        <form @submit="signup">
-            <div v-for="item in signupFormItems" :key="item.id">
-                <label :for="item.id">{{item.label}}</label>
-                <input :type="item.type" :id="item.id" :name="item.id" v-model="item.value"
-                :placeholder="item.placeholder">
-                <small style="color: crimson;"></small>
-            </div>
-            <button type="submit">Submit</button>
-        </form>
-    </div>
+  <div>
+    <form @submit="signup">
+      <div v-for="item in signupFormItems" :key="item.id">
+        <label :for="item.id">{{ item.label }}</label>
+        <input
+          :type="item.type"
+          :id="item.id"
+          :name="item.id"
+          v-model="item.value"
+          :placeholder="item.placeholder"
+        />
+        <small style="color: crimson;" v-if="item.error">{{
+          item.message
+        }}</small>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script>
-// import { UISignUpValidationService } from '../../services/validationService';
+import { UISignUpValidationService } from "../../services/validationService";
 
 export default {
-    name: "SignUp",
-    data() {
-        return {
-            signupFormItems: [
-                {
-                    label: "First Name",
-                    id: "firstName",
-                    value: "",
-                    placeholder: "First Name",
-                    type: "text",
-                    required: true,
-                    error: false,
-                    message: ""
-                },
-                {
-                    label: "Last Name",
-                    id: "lastName",
-                    value: "",
-                    placeholder: "Last Name",
-                    type: "text",
-                    required: true,
-                    error: false,
-                    message: ""
-                },
-                {
-                    label: "E-mail",
-                    id: "email",
-                    value: "",
-                    placeholder: "E-mail",
-                    type: "email",
-                    required: true,
-                    error: false,
-                    message: ""
-                },
-                {
-                    label: "Password",
-                    id: "password",
-                    value: "",
-                    placeholder: "Password",
-                    type: "password",
-                    required: true,
-                    error: false,
-                    message: ""
-                },
-                {
-                    label: "Confirm Password",
-                    id: "confirmPassword",
-                    value: "",
-                    placeholder: "Confirm Password",
-                    type: "password",
-                    required: true,
-                    error: false,
-                    message: ""
-                }
-            ]
-        }
+  name: "SignUp",
+  data() {
+    return {
+      signupFormItems: [
+        {
+          label: "First Name",
+          id: "firstName",
+          value: "",
+          placeholder: "First Name",
+          type: "text",
+          required: true,
+          error: false,
+          message: "",
+        },
+        {
+          label: "Last Name",
+          id: "lastName",
+          value: "",
+          placeholder: "Last Name",
+          type: "text",
+          required: true,
+          error: false,
+          message: "",
+        },
+        {
+          label: "E-mail",
+          id: "email",
+          value: "",
+          placeholder: "E-mail",
+          type: "email",
+          required: true,
+          error: false,
+          message: "",
+        },
+        {
+          label: "Password",
+          id: "password",
+          value: "",
+          placeholder: "Password",
+          type: "password",
+          required: true,
+          error: false,
+          message: "",
+        },
+        {
+          label: "Confirm Password",
+          id: "confirmPassword",
+          value: "",
+          placeholder: "Confirm Password",
+          type: "password",
+          required: true,
+          error: false,
+          message: "",
+        },
+      ],
+    };
+  },
+  methods: {
+    signup(e) {
+      e.preventDefault();
+
+      let validatedData = UISignUpValidationService(this.$data.signupFormItems);
+      this.updateChangedFields(validatedData);
     },
-    methods: {
-        signup(e){
-            e.preventDefault();
-            console.log(this.$data.signupFormItems[0].value)
-            // console.log(UISignUpValidationService(e.srcElement[0].value,e.srcElement[1].value,e.srcElement[2].value,e.srcElement[3].value, e.srcElement[4].value));
-            console.log(e.srcElement[0].validity.valid);
-            console.log(e);
-        }
+    updateChangedFields(data) {
+      data.map((value, index) => {
+        this.$data.signupFormItems[index].message = value.message;
+        this.$data.signupFormItems[index].error = value.error;
+      });
     },
-    setup () {
-        return {}
-    }
-}
+  },
+  setup() {
+    return {};
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/services/validationService.js
+++ src/services/validationService.js
@@ -3,16 +3,46 @@ const PASS_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 const EMAIL_REGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 
-export const UISignUpValidationService = (firstName, lastName, email, password, confirmPassword) => {
+export const UISignUpValidationService = (dataArray) => {
 
-    const errors = [];
+    const validatedDataArray = [];
+    const passwordValue = "";
 
-    if (firstName === "") errors.push({firstName: true, message: "Please enter your first name"});
-    else 
-    if (lastName === "") errors.push({lastName: true, message: "Please enter your last name"});
-    if (!email.match(EMAIL_REGEX)) errors.push({email: true, message: "Please enter a valid e-mail address"});
-    if (!password.match(PASS_REGEX)) errors.push({password: true, message: "Please enter a valid password"});
-    if (password !== confirmPassword || !confirmPassword.match(PASS_REGEX)) errors.push({confirmPassword: true, message: "Passwords do not match"})
+    dataArray.map((data) => {
+        const validatedData = data;
+        validatedData.error = false;
+        validatedData.message = "";
 
-    return errors;
+        if ((data.id === "firstName" || data.id === "lastName") && data.value === "") {
+
+            validatedData.error = true;
+            validatedData.message = `Please enter your ${data.label} correct!`;
+
+        } else if (data.id === "email" && !data.value.match(EMAIL_REGEX)) {
+
+            validatedData.error = true;
+            validatedData.message = `Please enter a valid ${data.label}!`;
+
+        } else if (data.id === "password" && !data.value.match(PASS_REGEX)) {
+
+            validatedData.error = true;
+            validatedData.message = `Please enter a valid ${data.label}!`;
+
+        } else if (data.id === "password" && data.value.match(PASS_REGEX)) {
+
+            passwordValue.concat(data.id.value);
+
+        } else if (data.id === "confirmPassword" && !data.value.match(PASS_REGEX)) {
+            validatedData.error = true;
+            validatedData.message = `Please enter a valid Password!`;
+
+            if (data.value !== passwordValue) {
+                validatedData.error = true;
+                validatedData.message = `Password do not match!`;
+            }
+        }
+        validatedDataArray.push(validatedData);
+    });
+
+    return validatedDataArray;
 };
