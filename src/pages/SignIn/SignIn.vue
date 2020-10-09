f796ee82c45e4de64715470b4a1f6efe6c938a72
Lazar Stankovic <lazar.stankovic@atessoft.rs>
2020-10-09 09:31:56 +0200

    Sign in page

--- src/pages/SignIn/SignIn.vue
+++ src/pages/SignIn/SignIn.vue
@@ -1,11 +1,67 @@
 <template>
-    <div>
-        Login
+    <div class="signup">
+    <div class="signup-container">
+      <h1>SIGN IN</h1>
+      <form @submit="signin">
+        <div v-for="item in signInFormItems" :key="item.id">
+          <input
+            :type="item.type"
+            :id="item.id"
+            :name="item.id"
+            v-model="item.value"
+            :placeholder="item.placeholder"
+          />
+          <small style="color: crimson" v-if="item.error">{{
+            item.message
+          }}</small>
+        </div>
+        <div class="d-flex flex-row justify-between align-baseline">
+            <div>
+                <input type="checkbox" id="remember-me" name="remember-me" class="mt-2" />
+                <label for="remember-me" class="ml-1">Remember me</label>
+            </div>
+            <a href="/forgot-password">Forgot password</a>
+        </div>
+        <button type="submit">SUBMIT</button>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
+            signInFormItems: [
+                {
+                    label: "E-mail",
+                    id: "email",
+                    value: "",
+                    placeholder: "Enter your E-mail",
+                    type: "email",
+                    required: true,
+                    error: false,
+                    message: "",
+                },
+                {
+                    label: "Password",
+                    id: "password",
+                    value: "",
+                    placeholder: "Enter your Password",
+                    type: "password",
+                    required: true,
+                    error: false,
+                    message: "",
+                }
+            ]
+        }
+    },
+    methods: {
+        signin(e){
+            e.preventDefault();
+            console.log(this.signInFormItems);
+        }
+    },
     setup () {
         
 
@@ -15,5 +71,58 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.mt-2 { margin-top: 1.5em !important; }
+.ml-1 { margin-left: .5em !important;}
+.d-flex { display: flex; }
+.flex-row { flex-direction: row; }
+.justify-between { justify-content: space-between; }
+.align-baseline { align-items: baseline; }
+.signup {
+  height: auto;
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 65px;
+  font-family: "Roboto", sans-serif !important;
+  font-size: 18px;
+}
+.signup-container {
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  justify-content: center;
+  align-self: center;
+  margin: 10px;
+}
+.signup-container form {
+  width: 100%;
+}
+.signup h1 {
+  font-size: 50px;
+  font-family: "Big Shoulders Display", cursive;
+  margin-bottom: -10px;
+}
 
+.signup input:not([type="checkbox"]) {
+  height: 50px;
+  width: calc(100% - 22px);
+  margin-top: 10px;
+  padding: 0 10px 0 10px;
+  font-family: "Roboto", sans-serif !important;
+  border: 1px solid #333;
+  border-radius: 4px;
+}
+.signup button {
+  height: 50px;
+  width: 100%;
+  font-size: 24px;
+  margin-top: 20px;
+  background: #333;
+  color: #fff;
+  letter-spacing: 6px;
+  font-family: "Roboto", sans-serif !important;
+  border: 1px solid transparent;
+  border-radius: 6px;
+}
 </style>
